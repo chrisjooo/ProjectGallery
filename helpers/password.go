b2d4b54149cf4031a7b0cf5805c357c2e30a23e8
Christian Jonathan <christianchrisjo@gmail.com>
2020-04-19 01:40:48 +0700

    added helpers for password

---
+++ helpers/password.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashAndSalt(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return string(hash)
+}
+
+func ComparePassword(hashedPwd string, plainPwd []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
