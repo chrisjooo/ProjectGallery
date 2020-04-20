806cdffe939925ed8f8b90e36127c9debcf6a4d4
Christian Jonathan <christianchrisjo@gmail.com>
2020-04-21 00:58:10 +0700

    added gitignore, updated main.go and conf

--- main.go
+++ main.go
@@ -2,10 +2,56 @@ package main
 
 import (
 	_ "ProjectGallery/routers"
+
 	"github.com/astaxie/beego"
+
+	"github.com/joho/godotenv"
+
+	"fmt"
+	"log"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := beego.AppConfig.String("mysqluser")
+	dbPwd := beego.AppConfig.String("mysqlpass")
+	dbName := beego.AppConfig.String("mysqldb")
+	dbUrls := beego.AppConfig.String("mysqlurls")
+	dbString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4", dbUser, dbPwd, dbUrls, dbName)
+
+	// Register Driver
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	// Register default database
+	orm.RegisterDataBase("default", "mysql", dbString)
+
+	// autosync
+	// db alias
+	name := "default"
+
+	// drop table and re-create
+	force := false
+
+	// print log
+	verbose := true
+
+	// error
+	err = orm.RunSyncdb(name, force, verbose)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -2,12 +2,28 @@ package routers
 
 import (
 	"ProjectGallery/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/test",
+			beego.NSInclude(
+				&controllers.MainController{},
+			),
+		),
+		beego.NSNamespace("/account",
+			beego.NSInclude(
+				&controllers.MainController{},
+			),
+		),
+		beego.NSNamespace("/project",
+			beego.NSInclude(
+				&controllers.MainController{},
+			),
+		),
+		beego.NSNamespace("/rating",
 			beego.NSInclude(
 				&controllers.MainController{},
 			),
