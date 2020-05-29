ba44d1c0d0eaee7bdf9894386ce2429cbe7cfe9f
Christian Jonathan <christianchrisjo@gmail.com>
2020-05-29 23:58:17 +0700

    routers comment

--- routers/commentsRouter_____________home_chrisjooo_go_src_ProjectGallery_controllers.go
+++ routers/commentsRouter_____________home_chrisjooo_go_src_ProjectGallery_controllers.go
@@ -61,6 +61,15 @@ func init() {
             Filters: nil,
             Params: nil})
 
+    beego.GlobalControllerRouter["ProjectGallery/controllers:AccountController"] = append(beego.GlobalControllerRouter["ProjectGallery/controllers:AccountController"],
+        beego.ControllerComments{
+            Method: "Logout",
+            Router: `/logout`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
     beego.GlobalControllerRouter["ProjectGallery/controllers:MainController"] = append(beego.GlobalControllerRouter["ProjectGallery/controllers:MainController"],
         beego.ControllerComments{
             Method: "Ping",
@@ -108,8 +117,8 @@ func init() {
 
     beego.GlobalControllerRouter["ProjectGallery/controllers:ProjectController"] = append(beego.GlobalControllerRouter["ProjectGallery/controllers:ProjectController"],
         beego.ControllerComments{
-            Method: "GetById",
-            Router: `/id/:id`,
+            Method: "GetLikeProjects",
+            Router: `/filter/like`,
             AllowHTTPMethods: []string{"get"},
             MethodParams: param.Make(),
             Filters: nil,
@@ -117,8 +126,8 @@ func init() {
 
     beego.GlobalControllerRouter["ProjectGallery/controllers:ProjectController"] = append(beego.GlobalControllerRouter["ProjectGallery/controllers:ProjectController"],
         beego.ControllerComments{
-            Method: "GetLikeProjects",
-            Router: `/like/`,
+            Method: "GetById",
+            Router: `/id/:id`,
             AllowHTTPMethods: []string{"get"},
             MethodParams: param.Make(),
             Filters: nil,
